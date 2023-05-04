90c5b42e951ca4fd3c1143bc3ac031802e8adbdd
Gaurav Anand <gaurav56anand@gmail.com>
2023-05-04 23:22:35 +0530

    Signup and Login Components and Footer Section along with Css module

---
+++ client/src/pages/signup/SignupPage.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import SignUp from '../../components/signUp/SignUp'
+import Nav from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
+
+function SignupPage() {
+  return (
+    <div>
+        <Nav/>
+        <SignUp/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default SignupPage
