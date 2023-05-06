44c19bba389fc54cb59b9c3e9051a65afe4c3573
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-05-07 00:04:21 +0530

    Doctor details page added

--- client/src/components/appointment/Appointment.tsx
+++ client/src/components/appointment/Appointment.tsx
@@ -1,73 +1,65 @@
 import styles from "./Appointment.module.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box, Button, FormControl, FormLabel, Heading, Input, Select, Stack, Textarea } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
-type FormValues = {
-    day: string;
-    date: Date;
-    doctor: string;
-    phone: string;
-    message: string;
-};
-
-const initialFormValues: FormValues = {
-    day: "",
-    date: new Date,
-    doctor: "",
-    phone: "",
-    message: "",
-};
-
 const Appointment = () => {
-    const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
+    const baseUrl = `https://jittery-shirt-tuna.cyclic.app`;
+    const [fetchData, setFetchData] = useState([]);
+    // const [time,setTime] = useState(0);
+    // const [time,setTime] = useState(0);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = event.target;
-        setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
-    };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("Form submitted:", formValues);
-    };
+
+    useEffect(()=>{
+        fetch(`${baseUrl}/`)
+    })
+
+   
+    useEffect(() => {
+        fetch(`${baseUrl}/doctors/all-doctors`)
+            .then(res => { return res.json() })
+            .then(data => {
+                setFetchData(data.doctors);
+            })
+            .catch(err => { alert(err) })
+    }, []);
+
 
     return (
-        <Box as="form" onSubmit={handleSubmit} p={6} bg="white" borderRadius="md" shadow="md" marginTop={"10vh"} textAlign={"center"}>
+        <Box as="form" p={6} bg="white" borderRadius="md" shadow="md" marginTop={"10vh"} textAlign={"center"}>
             <Heading as="h2" size="lg" mb={6}>
                 Health Care Appointment
             </Heading>
             <Stack spacing={4} width={{ base: '1xl', sm: '2xl', md: 'xl' }} margin={"auto"} className={styles.formContainer}>
-                <FormControl id="day" isRequired>
-                    <FormLabel fontWeight="bold">Appointment Day</FormLabel>
-                    {/* value={formValues.name} onChange={handleInputChange} */}
-                    <Select placeholder="Select Day" bg="gray.50" border="none" borderRadius="md">
-                        <option value="Monday">Monday</option>
-                        <option value="Tuesday">Tuesday</option>
-                        <option value="Wednesday">Wednesday</option>
-                        <option value="Thursday">Thursday</option>
-                        <option value="Friday">Friday</option>
-                        <option value="Saturday">Saturday</option>
-                        <option disabled value="0">Sunday (leave)</option>
-                    </Select>
-                </FormControl>
                 <FormControl id="date" isRequired>
                     <FormLabel fontWeight="bold">Appointment Date</FormLabel>
-                    <Input type="date" name="Appointment Date" onChange={handleInputChange} bg="gray.50" border="none" borderRadius="md" />
+                    <Input type="date" name="Appointment Date" bg="gray.50" border="none" borderRadius="md" />
                 </FormControl>
+                
                 <FormControl id="doctor" isRequired>
                     <FormLabel fontWeight="bold">Doctor's Name</FormLabel>
                     <Select placeholder="Select Doctor" bg="gray.50" border="none" borderRadius="md">
-                        <option value="Dr. Akshay">Dr. Akshay</option>
-                        <option value="Dr. Raj">Dr. Raj</option>
-                        <option value="Dr. Saurav">Dr. Saurav</option>
-                        <option value="Dr. Gaurav">Dr. Gaurav</option>
-                        <option value="dr. Vishal">dr. Vishal</option>
+                        {fetchData?.map((el, i) => {
+                            return (
+                                <option value={el["id"]} key={i}>{el["name"]}</option>
+                            )
+                        })}
+                    </Select>
+                </FormControl>
+                <FormControl id="Time" isRequired>
+                    <FormLabel fontWeight="bold">Time</FormLabel>
+                    <Select placeholder="Select Day" bg="gray.50" border="none" borderRadius="md">
+                        <option value="1 hr">10:00 AM - 11:00 AM</option>
+                        <option value="1 hr">11:30 AM - 12:30 PM</option>
+                        <option value="1 hr">01:00 PM - 02:00 PM</option>
+                        <option value="1 hr">02:30 PM - 03:30 PM</option>
+                        <option value="1 hr">04:00 PM - 05:00 PM</option>
                     </Select>
                 </FormControl>
                 <FormControl id="phone" isRequired>
                     <FormLabel fontWeight="bold">Phone</FormLabel>
-                    <Input type="tel" name="phone" value={formValues.phone} onChange={handleInputChange} bg="gray.50" border="none" borderRadius="md" />
+                    <Input type="tel" name="phone" bg="gray.50" border="none" borderRadius="md" />
                 </FormControl>
                 <FormControl id="ambulance" isRequired>
                     <FormLabel fontWeight="bold">Need Ambulance?</FormLabel>
@@ -78,7 +70,7 @@ const Appointment = () => {
                 </FormControl>
                 <FormControl id="message">
                     <FormLabel fontWeight="bold">Message</FormLabel>
-                    <Textarea name="message" value={formValues.message} onChange={handleInputChange} bg="gray.50" border="none" borderRadius="md" />
+                    <Textarea name="message" bg="gray.50" border="none" borderRadius="md" />
                 </FormControl>
                 <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
                     <Button type="submit" colorScheme="teal" bg="teal.500" borderRadius="md" _hover={{ bg: "teal.600" }}>
---
+++ client/src/pages/DoctorDetails/DotorDetailsPage.tsx
@@ -0,0 +1,15 @@
+import DoctorDetails from "../../components/doctorDetails/DoctorDetails"
+import Footer from "../../components/footer/Footer"
+import Navbar from "../../components/navbar/Navbar"
+
+function DotorDetailsPage() {
+  return (
+    <div>
+        <Navbar/>
+        <DoctorDetails/> 
+        <Footer/>
+    </div>
+  )
+}
+
+export default DotorDetailsPage
