d8fa92770d9808c2db7a003fab3622ba5a56e7f0
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-05-10 14:30:24 +0530

    vit.config file updated

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000 // Change the limit to 1000 kBs
+    chunkSizeWarningLimit: 5000 // Change the limit to 5000 kBs
   }
   // root:"./public",
   // base:"",
